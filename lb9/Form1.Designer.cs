4a39ff0b36e292fba039b1a5061237119a3dffb9
Stanislav22219 <128784563+Stanislav22219@users.noreply.github.com>
2024-04-25 15:41:06 +0300

    Add files via upload

---
+++ lb9/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace lb8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(937, 630);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(982, 123);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(272, 52);
+            this.btn_OK.TabIndex = 1;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(982, 70);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(272, 20);
+            this.txt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(977, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Значення параметру a";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1285, 680);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Побудова графіка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lb9/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lb8
+{
+    public partial class Form1 : Form
+    {
+        private double a;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "{0:0.00}";
+            chart1.ChartAreas[0].AxisY.LabelStyle.Format = "{0:0.00}";
+            chart1.ChartAreas[0].AxisX.Title = "x";
+            chart1.ChartAreas[0].AxisY.Title = "y";
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].Color = Color.Black;
+            chart1.Series[0].LegendText = "Графік функції";
+            chart1.Series[0].BorderWidth = 2;
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(txt.Text);
+            chart1.Series[0].Points.Clear();
+
+            for (double t = -Math.PI; t <= Math.PI; t += 0.01)
+            {
+                if (Math.Abs(t - Math.PI / 2) % Math.PI > 0.01)
+                {
+                    double x = a * Math.Tan(t);
+                    double y = a * Math.Pow(Math.Sin(t), 2);
+
+                    chart1.Series[0].Points.AddXY(x, y);
+                }
+            }
+        }
+    }
+}
