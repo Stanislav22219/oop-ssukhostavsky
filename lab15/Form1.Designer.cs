d8038e8f0867c63635f3da383ce681d3624aa617
Stanislav22219 <128784563+Stanislav22219@users.noreply.github.com>
2024-06-13 19:38:23 +0300

    Add files via upload

---
+++ lab15/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnDeleteFile = new System.Windows.Forms.Button();
+            this.btnDeleteLog = new System.Windows.Forms.Button();
+            this.btnCreateLog = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUpload = new System.Windows.Forms.TextBox();
+            this.txtDeleteFile = new System.Windows.Forms.TextBox();
+            this.txtDeleteLog = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnFileSize = new System.Windows.Forms.Button();
+            this.FadList = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Хост:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ім\'я користувача:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Пароль:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(111, 10);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(220, 20);
+            this.txtHost.TabIndex = 3;
+            this.txtHost.Text = "ftp://";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(111, 54);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(220, 20);
+            this.txtUser.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(111, 99);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(220, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(337, 13);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(112, 107);
+            this.btnGet.TabIndex = 6;
+            this.btnGet.Text = "Підключитися та отримати список файлів та каталогів";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 415);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Шлях:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(58, 412);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(391, 20);
+            this.txtPath.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUpload);
+            this.groupBox1.Controls.Add(this.btnDeleteFile);
+            this.groupBox1.Controls.Add(this.btnDeleteLog);
+            this.groupBox1.Controls.Add(this.btnCreateLog);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtUpload);
+            this.groupBox1.Controls.Add(this.txtDeleteFile);
+            this.groupBox1.Controls.Add(this.txtDeleteLog);
+            this.groupBox1.Controls.Add(this.txtLog);
+            this.groupBox1.Controls.Add(this.txtSize);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnFileSize);
+            this.groupBox1.Location = new System.Drawing.Point(19, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(430, 247);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Операції FTP";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(8, 203);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(180, 25);
+            this.btnUpload.TabIndex = 21;
+            this.btnUpload.Text = "Завантажити на FTP";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnDeleteFile
+            // 
+            this.btnDeleteFile.Location = new System.Drawing.Point(8, 158);
+            this.btnDeleteFile.Name = "btnDeleteFile";
+            this.btnDeleteFile.Size = new System.Drawing.Size(180, 25);
+            this.btnDeleteFile.TabIndex = 21;
+            this.btnDeleteFile.Text = "Видалити файл";
+            this.btnDeleteFile.UseVisualStyleBackColor = true;
+            this.btnDeleteFile.Click += new System.EventHandler(this.btnDeleteFile_Click);
+            // 
+            // btnDeleteLog
+            // 
+            this.btnDeleteLog.Location = new System.Drawing.Point(6, 110);
+            this.btnDeleteLog.Name = "btnDeleteLog";
+            this.btnDeleteLog.Size = new System.Drawing.Size(180, 25);
+            this.btnDeleteLog.TabIndex = 21;
+            this.btnDeleteLog.Text = "Видалити каталог";
+            this.btnDeleteLog.UseVisualStyleBackColor = true;
+            this.btnDeleteLog.Click += new System.EventHandler(this.btnDeleteLog_Click);
+            // 
+            // btnCreateLog
+            // 
+            this.btnCreateLog.Location = new System.Drawing.Point(6, 64);
+            this.btnCreateLog.Name = "btnCreateLog";
+            this.btnCreateLog.Size = new System.Drawing.Size(180, 25);
+            this.btnCreateLog.TabIndex = 20;
+            this.btnCreateLog.Text = "Створити каталог";
+            this.btnCreateLog.UseVisualStyleBackColor = true;
+            this.btnCreateLog.Click += new System.EventHandler(this.btnCreateLog_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(194, 211);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Каталог:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(194, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Файл:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(194, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Каталог:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(194, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Каталог:";
+            // 
+            // txtUpload
+            // 
+            this.txtUpload.Location = new System.Drawing.Point(251, 208);
+            this.txtUpload.Name = "txtUpload";
+            this.txtUpload.Size = new System.Drawing.Size(173, 20);
+            this.txtUpload.TabIndex = 15;
+            this.txtUpload.Text = "/";
+            // 
+            // txtDeleteFile
+            // 
+            this.txtDeleteFile.Location = new System.Drawing.Point(251, 161);
+            this.txtDeleteFile.Name = "txtDeleteFile";
+            this.txtDeleteFile.Size = new System.Drawing.Size(173, 20);
+            this.txtDeleteFile.TabIndex = 14;
+            this.txtDeleteFile.Text = "/";
+            // 
+            // txtDeleteLog
+            // 
+            this.txtDeleteLog.Location = new System.Drawing.Point(251, 113);
+            this.txtDeleteLog.Name = "txtDeleteLog";
+            this.txtDeleteLog.Size = new System.Drawing.Size(173, 20);
+            this.txtDeleteLog.TabIndex = 13;
+            this.txtDeleteLog.Text = "/";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(251, 67);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(173, 20);
+            this.txtLog.TabIndex = 12;
+            this.txtLog.Text = "/";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(251, 22);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(173, 20);
+            this.txtSize.TabIndex = 11;
+            this.txtSize.Text = "/";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Файл:";
+            // 
+            // btnFileSize
+            // 
+            this.btnFileSize.Location = new System.Drawing.Point(6, 19);
+            this.btnFileSize.Name = "btnFileSize";
+            this.btnFileSize.Size = new System.Drawing.Size(180, 25);
+            this.btnFileSize.TabIndex = 11;
+            this.btnFileSize.Text = "Розмір файлу";
+            this.btnFileSize.UseVisualStyleBackColor = true;
+            this.btnFileSize.Click += new System.EventHandler(this.btnFileSize_Click);
+            // 
+            // FadList
+            // 
+            this.FadList.FormattingEnabled = true;
+            this.FadList.HorizontalScrollbar = true;
+            this.FadList.Location = new System.Drawing.Point(455, 12);
+            this.FadList.Name = "FadList";
+            this.FadList.Size = new System.Drawing.Size(417, 420);
+            this.FadList.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.FadList);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUpload;
+        private System.Windows.Forms.TextBox txtDeleteFile;
+        private System.Windows.Forms.TextBox txtDeleteLog;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnFileSize;
+        private System.Windows.Forms.Button btnDeleteFile;
+        private System.Windows.Forms.Button btnDeleteLog;
+        private System.Windows.Forms.Button btnCreateLog;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ListBox FadList;
+    }
+}
+
---
+++ lab15/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {   
+            FadList.Items.Clear();
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtHost.Text);
+            request.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+            request.Method = Ftp.ListDirectoryDetails;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+
+
+            while (!reader.EndOfStream)
+            {
+                FadList.Items.Add(reader.ReadLine());
+
+            }
+            MessageBox.Show(response.WelcomeMessage);
+            reader.Close();
+            response.Close();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "Всі файли|*.*|png files|*.png|jpg files|*.jpg|gif " +
+                "files | *.gif | bmp files | *.bmp | exe files | *.exe | rar files | *.rar | zip files | *.zip | txt " +
+                "files | *.txt";
+                openFileDialog1.FilterIndex = 1;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = openFileDialog1.FileName;
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtHost.Text + txtUpload + openFileDialog1.SafeFileName);
+                    request.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+                    request.Method = Ftp.UploadFile;
+                    byte[] file = System.IO.File.ReadAllBytes(txtPath.Text);
+                    Stream strz = request.GetRequestStream();
+                    strz.Write(file, 0, file.Length);
+                    strz.Close();
+                    strz.Dispose();
+
+                    MessageBox.Show(openFileDialog1.SafeFileName + "завантажено");
+                }
+                else
+                {
+                    MessageBox.Show(openFileDialog1.SafeFileName + "Не заватажено");
+                }
+            }
+            catch(UriFormatException ex) 
+            {
+                String err = ex.Message;
+                MessageBox.Show(err);
+            }
+            
+        }
+
+        private void btnCreateLog_Click(object sender, EventArgs e)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtHost.Text + txtLog.Text);
+            request.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+            request.Method = Ftp.MakeDirectory;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            MessageBox.Show("Каталог " + txtLog.Text + "створено");
+
+        }//192.168.12.144
+
+        private void btnFileSize_Click(object sender, EventArgs a)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtHost.Text + txtSize.Text);
+            request.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+            request.Method = Ftp.GetFileSize;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            long size = response.ContentLength;
+            MessageBox.Show("Розмір файлу складає " + size/1000000 + " МБ");
+        }
+
+        private void btnDeleteFile_Click(object sender, EventArgs e)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtHost.Text + txtDeleteFile.Text);
+            request.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+            request.Method = Ftp.DeleteFile;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            MessageBox.Show("Файл" + txtDeleteFile.Text + " видалено");
+        }
+
+        private void btnDeleteLog_Click(object sender, EventArgs e)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtHost.Text + txtDeleteLog.Text);
+            request.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+            request.Method = Ftp.RemoveDirectory;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            MessageBox.Show("Каталог" + txtDeleteLog.Text + " видалено");
+        }
+    }
+}
