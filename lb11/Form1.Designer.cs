e7b83f55575256d380c432d6fbed56765c4af88c
Stanislav22219 <128784563+Stanislav22219@users.noreply.github.com>
2024-06-17 23:23:39 +0300

    Add files via upload

---
+++ lb11/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace lb11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtImg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgwDatabase = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.openfiledialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnImg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwDatabase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(99, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(99, 79);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(182, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(99, 144);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(182, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtImg
+            // 
+            this.txtImg.Location = new System.Drawing.Point(26, 668);
+            this.txtImg.Name = "txtImg";
+            this.txtImg.Size = new System.Drawing.Size(100, 20);
+            this.txtImg.TabIndex = 3;
+            this.txtImg.TextChanged += new System.EventHandler(this.txtImg_TextChanged);
+            this.txtImg.Leave += new System.EventHandler(this.txtImg_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Телефон";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email";
+            // 
+            // dgwDatabase
+            // 
+            this.dgwDatabase.AllowUserToAddRows = false;
+            this.dgwDatabase.AllowUserToDeleteRows = false;
+            this.dgwDatabase.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgwDatabase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwDatabase.Location = new System.Drawing.Point(26, 234);
+            this.dgwDatabase.Name = "dgwDatabase";
+            this.dgwDatabase.ReadOnly = true;
+            this.dgwDatabase.RowHeadersVisible = false;
+            this.dgwDatabase.RowHeadersWidth = 44;
+            this.dgwDatabase.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwDatabase.Size = new System.Drawing.Size(516, 408);
+            this.dgwDatabase.TabIndex = 7;
+            this.dgwDatabase.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwDatabase_CellEnter);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "name";
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "number";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.ColumnName = "email";
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.ColumnName = "img";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(288, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(185, 659);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 36);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Додати";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(320, 659);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 36);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Оновити";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(459, 659);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 36);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Видалити";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(100, 200);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(353, 20);
+            this.txtSearch.TabIndex = 12;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Пошук";
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(99, 295);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(206, 20);
+            this.txtID.TabIndex = 14;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(459, 187);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(83, 41);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Очистити поля";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // openfiledialog1
+            // 
+            this.openfiledialog1.FileName = "openFileDialog1";
+            // 
+            // btnImg
+            // 
+            this.btnImg.Location = new System.Drawing.Point(459, 27);
+            this.btnImg.Name = "btnImg";
+            this.btnImg.Size = new System.Drawing.Size(83, 154);
+            this.btnImg.TabIndex = 16;
+            this.btnImg.Text = "Додати зображення";
+            this.btnImg.UseVisualStyleBackColor = true;
+            this.btnImg.Click += new System.EventHandler(this.btnImg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 713);
+            this.Controls.Add(this.btnImg);
+            this.Controls.Add(this.dgwDatabase);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtImg);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Кафедра ЗВО";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwDatabase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtImg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgwDatabase;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        public System.Data.DataSet dataSet1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog openfiledialog1;
+        private System.Windows.Forms.Button btnImg;
+    }
+}
+
---
+++ lb11/Form1.cs
@@ -0,0 +1,202 @@
+﻿using lb11.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace lb11
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand cmd;
+        OleDbDataAdapter adapter;
+        DataTable dt;
+        string imgpath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\bin\\Debug\\Images\\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=MyDatabase.accdb");
+            dt = new DataTable();
+            adapter = new OleDbDataAdapter("SELECT *FROM TABLE1", conn);
+            conn.Open();
+            adapter.Fill(dt);
+            dgwDatabase.DataSource = dt;
+            conn.Close();
+            btnClear_Click(this, null);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            txtID.Text = null;
+            string query = "INSERT INTO TABLE1 (Name, [Phone number], Email, IMG) VALUES (@Name, [@Phone number], @Email, @IMG)";
+            cmd = new OleDbCommand(query);
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Phone number", txtNumber.Text);
+            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@IMG", txtImg.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Дані додані");
+            Form1_Load(this, null);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE TABLE1 SET Name=@Name, [Phone number]=[@Phone number], Email=@Email, IMG=@IMG WHERE ID=@ID";
+            cmd = new OleDbCommand(query);
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Phone number", txtNumber.Text);
+            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@IMG", txtImg.Text);
+            cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(txtID.Text));
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Дані оновлені");
+            Form1_Load(this, null);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM TABLE1 WHERE ID=@ID";
+            cmd = new OleDbCommand(query);
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(txtID.Text));
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Дані видалені");
+            Form1_Load(this, null);
+        }
+
+        private void dgwDatabase_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtName.Text = dgwDatabase.CurrentRow.Cells[0].Value.ToString();
+            txtNumber.Text = dgwDatabase.CurrentRow.Cells[1].Value.ToString();
+            txtEmail.Text = dgwDatabase.CurrentRow.Cells[2].Value.ToString(); 
+            txtImg.Text = dgwDatabase.CurrentRow.Cells[3].Value.ToString();
+            txtID.Text = dgwDatabase.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = null;
+            txtNumber.Text = null;
+            txtEmail.Text = null;
+            txtImg.Text = null;
+            txtID.Text = null;
+            txtSearch.Text = null;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = dt.DefaultView;
+            dv.RowFilter = "Name LIKE '%" + txtSearch.Text + "%'";
+            dgwDatabase.DataSource = dv;
+            txtName.Text = null;
+            txtNumber.Text = null;
+            txtEmail.Text = null;
+            txtImg.Text = null;
+            txtID.Text = null;
+        }
+        private void btnImg_Click(object sender, EventArgs e)
+        {
+            openfiledialog1.Title = "Виберіть ілюстрацію";
+            openfiledialog1.InitialDirectory = imgpath;
+            openfiledialog1.Filter = "фото|*.jpg|все файлы|*.*";
+            openfiledialog1.FileName = "";
+
+            if (openfiledialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openfiledialog1.FileName.ToLower().Contains(openfiledialog1.InitialDirectory.ToLower()) == true)
+                {
+                    txtImg.Text = openfiledialog1.SafeFileName;
+                }
+                else
+                {
+                    try
+                    {
+                        System.IO.File.Copy(openfiledialog1.FileName, imgpath + openfiledialog1.SafeFileName);
+                        txtImg.Text = openfiledialog1.SafeFileName;
+                    }
+                    catch (Exception ex) 
+                    {
+                        DialogResult dr;
+                        dr = MessageBox.Show(ex.Message + " Замінити його?", "", MessageBoxButtons.OKCancel, 
+                            MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+                        if (dr == DialogResult.OK)
+                        {
+                            System.IO.File.Copy(openfiledialog1.FileName, imgpath + openfiledialog1.SafeFileName, true);
+                            txtImg.Text = openfiledialog1.SafeFileName;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtImg_TextChanged(object sender, EventArgs e)
+        {
+            string imagefile;
+            string msg;
+            if (txtImg.Text.Length == 0)
+            {
+                imagefile = imgpath + "\\nobody.jpg";
+            }
+            else
+                imagefile = imgpath + txtImg.Text;
+            try
+            {
+                msg = "";
+                pictureBox1.Image = Image.FromFile(imagefile);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                msg = "File nof found: " + imagefile;
+                pictureBox1.Image = null;
+                pictureBox1.Refresh();
+                MessageBox.Show(msg);
+            }
+
+        }
+
+        private void txtImg_Leave(object sender, EventArgs e)
+        {
+            string imagefile;
+            string msg;
+            if (txtImg.Text.Length == 0)
+            {
+                imagefile = imgpath + "\\nobody.jpg";
+            }
+            else
+                imagefile = imgpath + txtImg.Text;
+            try
+            {
+                msg = "";
+                pictureBox1.Image = Image.FromFile(imagefile);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                msg = "File nof found: " + imagefile;
+                pictureBox1.Image = null;
+                pictureBox1.Refresh();
+                MessageBox.Show(msg);
+            }
+        }
+    }
+}
